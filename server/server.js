685c87d045ce58d3379de47f893882dc2f9270ff
Akshit945 <akshitgupta945@gmail.com>
2024-12-21 20:59:55 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -27,11 +27,14 @@ app.use('/api/products', productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/contact", contactRoutes);
 
+app.get('/', (req, res) => {
+  res.send("This is running");
+});
 
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
+ 
 
  
